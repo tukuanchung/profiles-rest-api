bb3002dbb3f1a7780311bc23495f0c1327c038f6
tukuanchung <tukuanchung@gmail.com>
2018-07-14 22:04:18 +0800

    Added our virst viewset and router.

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -1,6 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name="hello-viewset")
 
 urlpatterns = [
     url(r'^hello.view/',views.HelloApiView.as_view()),
+    url(r'', include(router.urls))
 ]
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -53,3 +54,17 @@ class HelloApiView(APIView):
         """Deletes an object."""
 
         return Response({'method':'delete'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet."""
+    def list(seld, request):
+        """Return a Hello message."""
+
+        a_viewset = [
+            "Uses actions (list, create, retrieve, update, partil_update)",
+            "Automatically maps to URLs using Routers",
+            "Provides mode functionality with less code."
+        ]
+
+        return Response({'message':'Hello!','a_viewset': a_viewset})
